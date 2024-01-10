a88cad8d7fc13c5ebee1cd6512596384e2a0b9f6
WeLearn1314 <dengjibin1412@gmail.com>
2024-01-10 21:53:04 +0800

    main

--- models.py
+++ models.py
@@ -41,6 +41,6 @@ def PMSDNet():
     model = Model(inputs=inpt, outputs=o)
     # model.summary() # print the size of model
     # x = torch.randn(1,1,50,50)
-    # flops, params = thop.profile(PMSDNet(),inputs=(x,)) #计算
+    # flops, params = thop.profile(PMSDNet(),inputs=(x,))
     # print(flops/1e9, params/1e6)
     return model
