fde368bd4ad0cbea804ed0e15a12bbb082495ff1
WeLearn1314 <dengjibin1412@gmail.com>
2024-01-13 11:54:58 +0800

    main

--- FOM-matlab-code-and-results/utilities/ComputeFOM.m
+++ FOM-matlab-code-and-results/utilities/ComputeFOM.m
@@ -1,22 +1,22 @@
 function fom = ComputeFOM(Ref_img, goal_img)
-a = 1/9.0; % 公式中比例常数
-edgeNum_Ref = length(find(Ref_img)); % 理想图像或者参考图像的边缘点数量
-edgeNum_goal = length(find(goal_img)); % 目标图像的边缘点数量
+a = 1/9.0; % Proportional constant in the formula
+edgeNum_Ref = length(find(Ref_img)); % Number of edge points in the reference image
+edgeNum_goal = length(find(goal_img)); % Number of edge points in the goal image
 
-MaxEdgeNum = max(edgeNum_Ref, edgeNum_goal); % 计算两图像中边缘点数量多的图像的数量
+MaxEdgeNum = max(edgeNum_Ref, edgeNum_goal); % Calculate the maximum number of edge points between the two images
 
-[row_Ref, col_Ref] = find(Ref_img); % 获取图像中不为0的点的坐标，也就是边缘点的坐标
+[row_Ref, col_Ref] = find(Ref_img); % Get the coordinates of non-zero points in the reference image, i.e., the edge points' coordinates
 [row_goal, col_goal] = find(goal_img);
 
-rowNum_Ref = size(Ref_img, 1); % 参考图像的行和列的数量
+rowNum_Ref = size(Ref_img, 1); % Number of rows in the reference image
 colNum_Ref = size(Ref_img, 2);
 
-d = sqrt(rowNum_Ref^2 + colNum_Ref^2) * ones(1, length(find(Ref_img))); % 图像的对角线像素数*（1，参考图像边缘点的数量）大小的全为1的矩阵，结果应该是值全为对角线值
-dd = zeros(1, length(find(goal_img))); % 创建一个（1，目标图像边缘点数量）大小的全为1的矩阵
+d = sqrt(rowNum_Ref^2 + colNum_Ref^2) * ones(1, length(find(Ref_img))); % Create a matrix of ones with size (1, number of edge points in the reference image) multiplied by the diagonal pixel count of the image, the result should be a matrix with all values equal to the diagonal value
+dd = zeros(1, length(find(goal_img))); % Create a matrix of zeros with size (1, number of edge points in the goal image)
 
 for i = 1:length(find(goal_img))
     for j = 1:length(find(Ref_img))
-        d(j) = (row_goal(i) - row_Ref(j))^2 + (col_goal(i) - col_Ref(j))^2; % 理想边缘点最近欧式距离d的平方值即为所求
+        d(j) = (row_goal(i) - row_Ref(j))^2 + (col_goal(i) - col_Ref(j))^2; % Squared Euclidean distance d from the nearest ideal edge point
     end
     dd(i) = min(d);
 end
